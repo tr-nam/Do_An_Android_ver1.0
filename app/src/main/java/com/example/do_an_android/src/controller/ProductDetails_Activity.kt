b92b15abac5b4c5d2ab20fa5ef525bb8596d835b
tr-nam <tr.namm29@gmail.com>
2024-11-13 21:00:54 +0700

    coding product details

---
+++ app/src/main/java/com/example/do_an_android/src/controller/ProductDetails_Activity.kt
@@ -0,0 +1,21 @@
+package com.example.do_an_android.src.controller
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.do_an_android.R
+
+class ProductDetails_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.layout_product_details)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
