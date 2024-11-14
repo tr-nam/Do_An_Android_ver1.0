72d380ef14bea7a1a9efa73db3244c59cd7206c6
tr-nam <tr.namm29@gmail.com>
2024-11-14 22:23:12 +0700

    test setEvent click

--- app/src/main/java/com/example/do_an_android/src/controller/Home_Activity.kt
+++ app/src/main/java/com/example/do_an_android/src/controller/Home_Activity.kt
@@ -1,14 +1,29 @@
 package com.example.do_an_android.src.controller
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.do_an_android.R
 
 class Home_Activity : AppCompatActivity() {
+    private lateinit var clickDetails: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.layout_home)
+        setControl()
+        setEvent()
+    }
+    private fun setControl() {
+        clickDetails = findViewById(R.id.imgProduct)
+    }
+    private fun setEvent() {
+        clickDetails.setOnClickListener() {
+            val intent = Intent(this, ProductDetails_Activity::class.java)
+            startActivity(intent)//Khởi chạy Activity mới
+        }
     }
 }
