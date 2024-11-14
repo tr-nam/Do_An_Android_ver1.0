5024bf7e0da5be147b477509d06ced35494fc1db
tr-nam <tr.namm29@gmail.com>
2024-11-14 21:58:18 +0700

    coding product details

--- app/src/main/java/com/example/do_an_android/src/controller/ProductDetails_Activity.kt
+++ app/src/main/java/com/example/do_an_android/src/controller/ProductDetails_Activity.kt
@@ -12,10 +12,5 @@ class ProductDetails_Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.layout_product_details)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
